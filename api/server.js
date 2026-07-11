b547332aa73ee9e1c797f7303dba9734e8629533
mrzaw2024 <your-email@gmail.com>
2026-07-11 21:14:41 +0630

    Add 1.html as index.html with Firebase Auth

--- api/server.js
+++ api/server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// Serve static files from public directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Handle all routes - serve index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
