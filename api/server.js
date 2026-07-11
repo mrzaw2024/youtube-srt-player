81ed31621e8b44670b1f372eebdabb83da5aa908
mrzaw2024 <your-email@gmail.com>
2026-07-11 21:22:14 +0630

    Complete: 1.html with Firebase Auth + UTF-8 Encoding

--- api/server.js
+++ api/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Set charset for all responses
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req, res) => {
